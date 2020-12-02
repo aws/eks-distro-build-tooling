fd894d37b0c6c57d28cadc1b518ffae59390accc
Abhay Krishna <arnchlm@amazon.com>
2020-12-02 03:07:49 -0800

    Update check_update.sh

--- eks-distro-base/check_update.sh
+++ eks-distro-base/check_update.sh
@@ -24,7 +24,7 @@ if [ $? -eq 0 ]; then
     export TZ=America/Los_Angeles
     DATE_EPOCH=$(date "+%F-%s")
     make release -C eks-distro-base DEVELOPMENT=false IMAGE_TAG=${DATE_EPOCH}
-    bash ./eks-distro-base/create_pr.sh eks-distro-build-tooling '.*' ${DATE_EPOCH} eks-distro-base/TAG_FILE
+    bash ./eks-distro-base/create_pr.sh eks-distro-build-tooling '.*' ${DATE_EPOCH} TAG_FILE
     bash ./eks-distro-base/create_pr.sh eks-distro 'BASE_TAG?=.*' 'BASE_TAG?='"${DATE_EPOCH}" Makefile
-    bash ./eks-distro-base/create_pr.sh eks-distro-prow-jobs '\(eks-distro/base\):.*' '\1:'"${DATE_EPOCH}" jobs/aws/eks-distro-build-tooling/eks-distro-base-periodics.yaml
+    bash ./eks-distro-base/create_pr.sh eks-distro-prow-jobs '\(eks-distro/base\):.*' '\1:'"${DATE_EPOCH}" eks-distro-base-periodics.yaml
 fi
