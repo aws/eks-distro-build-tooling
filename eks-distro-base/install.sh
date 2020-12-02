6cbc02e5bea2288dc7c55505021bcf68e33a4940
Abhay Krishna <arnchlm@amazon.com>
2020-12-02 15:39:03 -0800

    Update install.sh

--- eks-distro-base/install.sh
+++ eks-distro-base/install.sh
@@ -18,21 +18,8 @@ set -e
 set -o pipefail
 set -x
 
-amazon-linux-extras enable docker
-yum install -y \
-    awscli \
-    amazon-ecr-credential-helper \
-    git \
-    make \
-    openssh-clients \
-    tar \
-    wget \
 
-BUILDKIT_VERSION="${BUILDKIT_VERSION:-v0.7.2}"
-wget --progress dot:giga https://github.com/moby/buildkit/releases/download/$BUILDKIT_VERSION/buildkit-$BUILDKIT_VERSION.linux-amd64.tar.gz
-sha256sum -c builder-base/buildkit-checksum
-tar -C /usr -xzf buildkit-$BUILDKIT_VERSION.linux-amd64.tar.gz
-rm -rf buildkit-$BUILDKIT_VERSION.linux-amd64.tar.gz
+yum install -y openssh-clients
 
 GITHUB_CLIENT_VERSION="${GITHUB_CLIENT_VERSION:-1.2.1}"
 wget --progress dot:giga https://github.com/cli/cli/releases/download/v1.2.1/gh_1.2.1_linux_amd64.tar.gz
