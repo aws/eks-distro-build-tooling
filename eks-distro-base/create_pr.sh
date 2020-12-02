666fc84f4321c632ad36cc0fa857d4007a807b7c
Abhay Krishna <arnchlm@amazon.com>
2020-12-02 10:39:39 -0800

    Update create_pr.sh

--- eks-distro-base/create_pr.sh
+++ eks-distro-base/create_pr.sh
@@ -18,8 +18,6 @@ set -e
 set -o pipefail
 set -x
 
-TOKEN=$(cat /secrets/github/token)
-
 REPO="$1"
 OLD_TAG="$2"
 NEW_TAG="$3"
@@ -36,31 +34,31 @@ fi
 PR_TITLE="Update base image tag in ${FILENAME}"
 PR_BODY="This PR updates the base image tag in ${FILENAME} with \
 the tag of the newly-built EKS Distro base image"
-REVIEWERS=(
-  "micahhausler"
-  "vivek-koppuru"
-)
 
 cd ../${REPO}
 git remote add upstream https://github.com/aws/${REPO}.git
-git remote add origin https://abhay-krishna:${TOKEN}@github.com/abhay-krishna/${REPO}.git
+git remote add origin https://abhay-krishna@github.com/abhay-krishna/${REPO}.git
+git config credential-helper 'store --file /secrets/github/token'
 #git fetch upstream
 #git rebase upstream/main
-git checkout -b image-update-branch
+git checkout -b image-tag-update
 
 for FILE in $(find ./ -name $FILEPATH); do
     if [ $REPO = "eks-distro" ]; then
         if [ $(dirname $FILE) = "." ]; then
             OLD_TAG="^BASE_IMAGE?=\(.*\):.*"
             NEW_TAG="BASE_IMAGE?=\1:${DATE_EPOCH}"
+        else
+            OLD_TAG="$2"
+            NEW_TAG="$3"
         fi
     fi
     sed -i "s,${OLD_TAG},${NEW_TAG}," $FILE
     git add $FILE
 done
 git commit -m "Update EKS Distro base image tag"
-git push -u origin image-update-branch -f
+git push -u origin image-tag-update -f
 
-echo $TOKEN | gh auth login --with-token
+gh auth login --with-token < /secrets/github/token
 IFS=,
-gh pr create --title $PR_TITLE --body $PR_BODY --reviewer "${REVIEWERS[*]}" --draft
+gh pr create --title $PR_TITLE --body $PR_BODY --draft
