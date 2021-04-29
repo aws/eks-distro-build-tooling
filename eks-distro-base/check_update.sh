f69ab71638773e6720680a44655ed69260e91929
Abhay Krishna <arnchlm@amazon.com>
2021-04-30 05:14:16 +0530

    Remove DEVELOPMENT var from build-tooling projet Makefiles (#153)

--- eks-distro-base/check_update.sh
+++ eks-distro-base/check_update.sh
@@ -20,12 +20,10 @@ set -x
 
 SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
-IMAGE_REPO=$1
-IMAGE_NAME=$2
-IMAGE_TAG=$3
-DRY_RUN_FLAG=$4
+IMAGE_TAG=$1
+DRY_RUN_FLAG=$2
 
-BASE_IMAGE=${IMAGE_REPO}/${IMAGE_NAME}:$(cat ${SCRIPT_ROOT}/../EKS_DISTRO_BASE_TAG_FILE)
+BASE_IMAGE=public.ecr.aws/eks-distro-build-tooling/eks-distro-base:$(cat ${SCRIPT_ROOT}/../EKS_DISTRO_BASE_TAG_FILE)
 mkdir check-update
 cat << 'EOF' >> check-update/Dockerfile
 FROM $BASE_IMAGE AS base_image
