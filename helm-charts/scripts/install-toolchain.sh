0580fd97eae6ec59dc4b6a29331a1c12ded15abc
Abhay Krishna <arnchlm@amazon.com>
2021-11-05 17:57:31 -0700

    Build multiple Golang versions only for amd64 version (#252) (#250)

--- helm-charts/scripts/install-toolchain.sh
+++ helm-charts/scripts/install-toolchain.sh
@@ -23,30 +23,42 @@ ARCH=$([[ $(uname -m) = "x86_64" ]] && echo 'amd64' || echo 'arm64')
 TMP_DIR="${BUILD_DIR}/tmp"
 mkdir -p "${TOOLS_DIR}"
 
-HELM_VERSION="v3.4.1"
-KUBECTL_VERSION=v1.19.6
-KIND_VERSION=v0.5.1
-RELEASE_BRANCH=1-19
-RELEASE=1
+HELM_VERSION=v3.7.1
+KUBECTL_VERSION=v1.20.7
+KIND_VERSION=v0.11.1
+RELEASE_BRANCH=1-20
+RELEASE=8
 
 ## Install kubectl
-curl -sSL "https://distro.eks.amazonaws.com/kubernetes-${RELEASE_BRANCH}/releases/${RELEASE}/artifacts/kubernetes/${KUBECTL_VERSION}/bin/linux/amd64/kubectl" -o "${TOOLS_DIR}/kubectl"
-chmod +x "${TOOLS_DIR}/kubectl"
+if ! command -v kubectl &> /dev/null; then
+    echo "kubectl could not be found. Downloading..."
+    curl -sSL "https://distro.eks.amazonaws.com/kubernetes-${RELEASE_BRANCH}/releases/${RELEASE}/artifacts/kubernetes/${KUBECTL_VERSION}/bin/linux/amd64/kubectl" -o "${TOOLS_DIR}/kubectl"
+    chmod +x "${TOOLS_DIR}/kubectl"
+fi
 
 ## Install kubeval
-mkdir -p "${TMP_DIR}/kubeval"
-curl -sSL https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-${PLATFORM}-${ARCH}.tar.gz | tar xz -C "${TMP_DIR}/kubeval"
-mv "${TMP_DIR}/kubeval/kubeval" "${TOOLS_DIR}/kubeval"
+if ! command -v kubeval &> /dev/null; then
+    echo "kubeval could not be found. Downloading..."
+    mkdir -p "${TMP_DIR}/kubeval"
+    curl -sSL https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-${PLATFORM}-${ARCH}.tar.gz | tar xz -C "${TMP_DIR}/kubeval"
+    mv "${TMP_DIR}/kubeval/kubeval" "${TOOLS_DIR}/kubeval"
+fi
 
 ## Install helm
-mkdir -p "${TMP_DIR}/helm"
-curl -sSL https://get.helm.sh/helm-${HELM_VERSION}-${PLATFORM}-${ARCH}.tar.gz | tar xz -C "${TMP_DIR}/helm"
-mv "${TMP_DIR}/helm/${PLATFORM}-${ARCH}/helm" "${TOOLS_DIR}/helm"
-rm -rf "${PLATFORM}-${ARCH}"
+if ! command -v helm &> /dev/null; then
+    echo "helm could not be found. Downloading..."
+    mkdir -p "${TMP_DIR}/helm"
+    curl -sSL https://get.helm.sh/helm-${HELM_VERSION}-${PLATFORM}-${ARCH}.tar.gz | tar xz -C "${TMP_DIR}/helm"
+    mv "${TMP_DIR}/helm/${PLATFORM}-${ARCH}/helm" "${TOOLS_DIR}/helm"
+    rm -rf "${PLATFORM}-${ARCH}"
+fi
 
 ## Install kind
-curl -sSL "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-${PLATFORM}-${ARCH}" -o "${TOOLS_DIR}/kind"
-chmod +x "${TOOLS_DIR}/kind"
+if ! command -v kind &> /dev/null; then
+    echo "kind could not be found. Downloading..."
+    curl -sSL "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-${PLATFORM}-${ARCH}" -o "${TOOLS_DIR}/kind"
+    chmod +x "${TOOLS_DIR}/kind"
+fi
 
 rm -rf ${TMP_DIR}
 
--- helm-charts/scripts/verify-version.sh
+++ helm-charts/scripts/verify-version.sh
@@ -28,29 +28,40 @@ git fetch $REMOTE_URL $PREV_RELEASE_HASH
 EXIT_CODE=0
 
 cd $CHARTS_DIR
-for d in */; do
-  if git diff-index ${PREV_RELEASE_HASH} --quiet -- $d/templates $d/values.yaml --; then
-    echo "✅ $d has no changes since last release"
+for chart in *; do
+  DIFF_CHECK_TARGETS="$chart/templates $chart/values.yaml"
+  if [ $chart = "amazon-eks-pod-identity-webhook" ]; then
+    DIFF_CHECK_TARGETS="$chart/config $DIFF_CHECK_TARGETS"
+  fi
+  CHART_EXISTED=$(git ls-tree -r ${PREV_RELEASE_HASH} --name-only | grep -c ${chart}/Chart.yaml || true)
+  if [ $CHART_EXISTED -eq 0 ]; then
+    echo "✅ This is the first release of chart $chart, nothing to compare"
+  elif git diff-index ${PREV_RELEASE_HASH} --quiet -- $DIFF_CHECK_TARGETS --; then
+    echo "✅ Chart $chart has no changes since last release"
   else
-    CURR_VERSION=$(grep "version:" $d/Chart.yaml | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
-    PREV_VERSION=$(git show ${PREV_RELEASE_HASH}:helm-charts/stable/${d}Chart.yaml | grep "version:" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
+    CURR_VERSION=$(grep "version:" $chart/Chart.yaml | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
+    PREV_VERSION=$(git show ${PREV_RELEASE_HASH}:helm-charts/stable/${chart}/Chart.yaml | grep "version:" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
     TEMPLATES_CHANGED=false
     if [ "${CURR_VERSION}" = "${PREV_VERSION}" ]; then
-      for file in $d/templates/* $d/values.yaml; do
-        CHANGED_LINES=$(git show -U0 $file | grep '^[+-]' | grep -Ev '^(--- a/|\+\+\+ b/|\+#|-#)' || true)
+      FILES_TO_CHECK="$chart/templates/* $chart/values.yaml"
+      if [ $chart = "amazon-eks-pod-identity-webhook" ]; then
+        FILES_TO_CHECK="$chart/config/* $FILES_TO_CHECK"
+      fi
+      for file in $FILES_TO_CHECK; do
+        CHANGED_LINES=$(git show -U0 $file | grep '^[+-]' | grep -Ev '^(--- a/|\+\+\+ b/|\+#|-#|\+$)' || true)
         if [ "$CHANGED_LINES" != "" ]; then
           TEMPLATES_CHANGED=true
           break
         fi
       done
       if [ "$TEMPLATES_CHANGED" = "true" ]; then
-        echo "❌ $d has the same Chart version as the last release $PREV_VERSION, but templates have been modified"
+        echo "❌ Chart $chart has the same Chart version as the last release $PREV_VERSION, but templates have been modified"
         EXIT_CODE=1
       else
-        echo "✅ $d has the same Chart version as the last release $PREV_VERSION and templates have not been modified"
+        echo "✅ Chart $chart has the same Chart version as the last release $PREV_VERSION and templates have not been modified"
       fi
     else 
-      echo "✅ $d has a different version since the last release ($PREV_VERSION -> $CURR_VERSION)"
+      echo "✅ Chart $chart has a different Chart version since the last release ($PREV_VERSION -> $CURR_VERSION)"
     fi
   fi
 done
