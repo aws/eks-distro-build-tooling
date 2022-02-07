f006b40b5bc57d73add9c05fffa1173932c7cbf8
Jackson West <jgw@amazon.com>
2022-02-07 09:53:42 -0600

    support eks-d 1.22 release and fix snapshotter

--- release/pkg/assets_snapshotter.go
+++ release/pkg/assets_snapshotter.go
@@ -25,7 +25,7 @@ import (
 // GetSnapshotterComponent returns the Component for External Snapshotter
 func (r *ReleaseConfig) GetSnapshotterComponent(spec distrov1alpha1.ReleaseSpec) (*distrov1alpha1.Component, error) {
 	projectSource := "projects/kubernetes-csi/external-snapshotter"
-	tagFile := filepath.Join(r.BuildRepoSource, projectSource, "GIT_TAG")
+	tagFile := filepath.Join(r.BuildRepoSource, projectSource, spec.Channel, "GIT_TAG")
 	gitTag, err := readTag(tagFile)
 	if err != nil {
 		return nil, errors.Cause(err)
