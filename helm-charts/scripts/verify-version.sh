7ec5f8a6142d1a1c9afe2ada349d50cfc9a2fcec
Abhay Krishna Arunachalam <arnchlm@amazon.com>
2022-03-30 10:58:34 -0700

    Fix Deck service ports mapping

--- helm-charts/scripts/verify-version.sh
+++ helm-charts/scripts/verify-version.sh
@@ -30,6 +30,9 @@ EXIT_CODE=0
 cd $CHARTS_DIR
 for chart in *; do
   DIFF_CHECK_TARGETS="$chart/templates $chart/values.yaml"
+  if [ $chart = "prow-control-plane" ]; then
+    DIFF_CHECK_TARGETS="$GIT_REPO_ROOT/helm-charts/patches/prow-control-plane"
+  fi
   if [ $chart = "amazon-eks-pod-identity-webhook" ]; then
     DIFF_CHECK_TARGETS="$chart/config $DIFF_CHECK_TARGETS"
   fi
