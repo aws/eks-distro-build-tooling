302f807a76dd23c56b8d3ab1e4c8a1ebedfedcbf
Kirsten Schumy <ksschumy@amazon.com>
2022-08-02 18:47:50 -0700

    Added channel to cni

--- release/pkg/assets_cni.go
+++ release/pkg/assets_cni.go
@@ -25,7 +25,7 @@ import (
 // GetCniComponent returns the Component for CNI plugins
 func (r *ReleaseConfig) GetCniComponent(spec distrov1alpha1.ReleaseSpec) (*distrov1alpha1.Component, error) {
 	projectSource := "projects/containernetworking/plugins"
-	tagFile := filepath.Join(r.BuildRepoSource, projectSource, "GIT_TAG")
+	tagFile := filepath.Join(r.BuildRepoSource, projectSource, spec.Channel, "GIT_TAG")
 	gitTag, err := readTag(tagFile)
 	if err != nil {
 		return nil, errors.Cause(err)
