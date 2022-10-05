050698a6f5f1771e004a799e55a45dc492bfc6ec
Cameron Rozean <rcrozean@amazon.com>
2022-10-05 10:22:55 -0700

    file restructuring and adding root file

---
+++ tools/slim-jim/cmd/ifal2.go
@@ -0,0 +1,14 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var ifal2Cmd = &cobra.Command{
+	Use: "if-al2",
+	Short: "If OS AL2",
+	Long: "",
+}
+
--- tools/slim-jim/cmd/main.go
+++ tools/slim-jim/cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
-import {
+import ( 
 	"github.com/aws/eks-distro-build-tooling/slim-jim/cmd/slim-jim/cmd"
 	"os"
-}
+)
 
 func main() {
 	if cmd.Execute() == nil {
