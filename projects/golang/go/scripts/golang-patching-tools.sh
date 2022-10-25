14dca56055dfb1584773df82dedf55a941ddce91
Cameron Rozean <rcrozean@amazon.com>
2022-10-25 06:50:56 -0700

    transfer more of the scripts to tools.sh script

--- projects/golang/go/scripts/golang-patching-tools.sh
+++ projects/golang/go/scripts/golang-patching-tools.sh
@@ -8,6 +8,8 @@ PROJECT_DIRECTORY="$BASE_DIRECTORY/projects/golang/go/"
 
 GO_REPO="$(dirname "$BASE_DIRECTORY")/go"
 
+GO_VERSIONS=('1.15.15', '1.16.15', '1.17.13')
+
 clone-go() {
 	if [[ ! -e $GO_REPO ]]; then
 		git clone "$GO_REPO_URL" "$(dirname "$GO_REPO")"
@@ -15,11 +17,15 @@ clone-go() {
 }
 
 create-eks-patch-branches() {
-	echo "create-eks-patch-branches"
+	for ver in "${GO_VERSION[@]}"; do
+		git checkout "release-branch.go${ver:0:4}"
+		git checkout -B "go-$ver-eks"
+		git am $PROJECTS_DIRECTORY/${ver:0:4}/patches/*.patch
+	done	
 }
 
 cherry-pick-commit() {
-	echo "cherry-pick-commit"
+	git cherry-pick $1
 }
 
 create-patch() {
