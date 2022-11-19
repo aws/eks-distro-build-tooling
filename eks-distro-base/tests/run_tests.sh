7e7f5015ded6747d9da4618defb7af2dce60294d
Jackson West <jgw@amazon.com>
2022-11-19 13:51:06 -0600

    Switches eks-d golang image for minimal images tests

--- eks-distro-base/tests/run_tests.sh
+++ eks-distro-base/tests/run_tests.sh
@@ -27,6 +27,28 @@ LOCAL_REGISTRY=$6
 
 SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
+if [ "$AL_TAG" = "2022" ]; then
+    AL_TAG="22"
+fi
+
+function retry() {
+  local n=1
+  local max=120
+  local delay=5
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        sleep $delay;
+      fi
+    }
+  done
+}
+
+function build::docker::retry_pull() {
+  retry docker pull "$@"
+}
+
 function check_base() {
     $SCRIPT_ROOT/../../scripts/buildkit.sh  \
         build \
@@ -39,10 +61,16 @@ function check_base() {
         --opt target=check-base \
         --local dockerfile=./ \
 		--local context=./tests \
-		--output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/base-test:latest\",push=true
+        --export-cache type=inline \
+        --import-cache type=registry,ref=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:base-latest \
+        --output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:base-latest\",push=true
 
     for platform in ${PLATFORMS//,/ }; do
-        docker run --rm --platform=$platform $LOCAL_REGISTRY/base-test:latest
+        build::docker::retry_pull --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:base-latest
+        if ! docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:base-latest; then
+            echo "base issue!"
+            exit 1
+        fi
     done
 }
 
@@ -62,10 +90,13 @@ function check_base-glibc() {
         --opt target=check-cgo \
         --local dockerfile=./ \
 		--local context=./tests \
-		--output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/base-test:glibc-latest\",push=true
-    
+        --export-cache type=inline \
+        --import-cache type=registry,ref=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:glibc-latest \
+        --output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:glibc-latest\",push=true
+
     for platform in ${PLATFORMS//,/ }; do
-        if docker run --rm --platform=$platform $LOCAL_REGISTRY/base-test:glibc-latest | grep -v 'Printed from unsafe C code'; then
+        build::docker::retry_pull --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:glibc-latest
+        if docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:glibc-latest | grep -v 'Printed from unsafe C code'; then
             echo "glibc issue!"
             exit 1
         fi
@@ -84,8 +115,10 @@ function check_base-iptables() {
         --opt target=check-iptables-legacy \
         --local dockerfile=./ \
 		--local context=./tests \
-		--output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/base-test:iptables-legacy-latest\",push=true
-    
+        --export-cache type=inline \
+        --import-cache type=registry,ref=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-legacy-latest \
+        --output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-legacy-latest\",push=true
+
     $SCRIPT_ROOT/../../scripts/buildkit.sh  \
         build \
         --frontend dockerfile.v0 \
@@ -97,23 +130,27 @@ function check_base-iptables() {
         --opt target=check-iptables-nft \
         --local dockerfile=./ \
 		--local context=./tests \
-		--output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/base-test:iptables-nft-latest\",push=true
-    
+        --export-cache type=inline \
+        --import-cache type=registry,ref=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-nft-latest \
+        --output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-nft-latest\",push=true
+
     for platform in ${PLATFORMS//,/ }; do
-        if docker run --rm --platform=$platform --pull=always $LOCAL_REGISTRY/base-test:iptables-legacy-latest iptables --version | grep -v 'legacy'; then
+        build::docker::retry_pull --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-legacy-latest
+
+        if docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-legacy-latest iptables --version | grep -v 'legacy'; then
             echo "iptables legacy issue!"
             exit 1
         fi
 
-        if docker run --rm --platform=$platform --pull=always $LOCAL_REGISTRY/base-test:iptables-legacy-latest ip6tables --version | grep -v 'legacy'; then
+        if docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-legacy-latest ip6tables --version | grep -v 'legacy'; then
             echo "ip6tables legacy issue!"
             exit 1
         fi
-        if ! docker run --rm --platform=$platform --pull=always $LOCAL_REGISTRY/base-test:iptables-legacy-latest iptables-save; then
+        if ! docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-legacy-latest iptables-save; then
             echo "iptables-save legacy issue!"
             exit 1
         fi
-        if ! docker run --rm --platform=$platform --pull=always $LOCAL_REGISTRY/base-test:iptables-legacy-latest ip6tables-save; then
+        if ! docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-legacy-latest ip6tables-save; then
             echo "ip6tables-save legacy issue!"
             exit 1
         fi
@@ -123,20 +160,22 @@ function check_base-iptables() {
         if [[ ! "$platform" = "linux/$(go env GOHOSTARCH)" ]]; then
             continue
         fi
+        
+        build::docker::retry_pull --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-nft-latest
 
-        if docker run --rm --platform=$platform --pull=always $LOCAL_REGISTRY/base-test:iptables-nft-latest iptables --version | grep -v 'nf_tables'; then
+        if docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-nft-latest iptables --version | grep -v 'nf_tables'; then
             echo "iptables nft issue!"
             exit 1
         fi
-        if docker run --rm --platform=$platform --pull=always $LOCAL_REGISTRY/base-test:iptables-nft-latest ip6tables --version | grep -v 'nf_tables'; then
+        if docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-nft-latest ip6tables --version | grep -v 'nf_tables'; then
             echo "ip6tables nft issue!"
             exit 1
         fi
-        if ! docker run --rm --platform=$platform --pull=always $LOCAL_REGISTRY/base-test:iptables-nft-latest ebtables --version; then
+        if ! docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-nft-latest ebtables --version; then
             echo "ebtables nft issue!"
             exit 1
         fi
-        if ! docker run --rm --platform=$platform --pull=always $LOCAL_REGISTRY/base-test:iptables-nft-latest arptables --version; then
+        if ! docker run --rm --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:iptables-nft-latest arptables --version; then
             echo "arptables nft issue!"
             exit 1
         fi
@@ -145,7 +184,8 @@ function check_base-iptables() {
 
 function check_base-csi() {
     for platform in ${PLATFORMS//,/ }; do
-        if docker run --rm --platform=$platform --pull=always $IMAGE_REPO/eks-distro-minimal-base-csi:$IMAGE_TAG xfs_info -V | grep -v 'xfs_info version'; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-csi:$IMAGE_TAG
+        if docker run --rm --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-csi:$IMAGE_TAG xfs_info -V | grep -v 'xfs_info version'; then
             echo "csi xfs issue!"
             exit 1
         fi
@@ -154,7 +194,8 @@ function check_base-csi() {
 
  function check_base-csi-ebs() {
     for platform in ${PLATFORMS//,/ }; do
-        if docker run --rm --platform=$platform --pull=always $IMAGE_REPO/eks-distro-minimal-base-csi-ebs:$IMAGE_TAG mount --version | grep -v 'mount'; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-csi-ebs:$IMAGE_TAG
+        if docker run --rm --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-csi-ebs:$IMAGE_TAG mount --version | grep -v 'mount'; then
             echo "csi xfs issue!"
             exit 1
         fi
@@ -191,25 +232,28 @@ function check_base-csi() {
         --opt target=check-git \
         --local dockerfile=./ \
 		--local context=./tests $netrc \
-		--output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/base-test:git-latest\",push=true
-    
+        --export-cache type=inline \
+        --import-cache type=registry,ref=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:git-latest \
+        --output type=image,oci-mediatypes=true,\"name=$LOCAL_REGISTRY/eks-distro-minimal-images-base-test:git-latest\",push=true
+
     for platform in ${PLATFORMS//,/ }; do
         # use git cli to clone private and public repo
-        docker run --rm --platform=$platform --pull=always -v $SSH_KEY_FOLDER/id_rsa:/root/.ssh/id_rsa \
+        build::docker::retry_pull --platform=$platform $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:git-latest
+        docker run -it --platform=$platform -v $SSH_KEY_FOLDER/id_rsa:/root/.ssh/id_rsa \
             -v $SSH_KEY_FOLDER/id_rsa.pub:/root/.ssh/id_rsa.pub \
             -v $SSH_KEY_FOLDER/known_hosts:/root/.ssh/known_hosts \
-            $LOCAL_REGISTRY/base-test:git-latest git clone $PRIVATE_REPO
+            $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:git-latest git clone $PRIVATE_REPO
 
-        docker run --rm --platform=$platform --pull=always -v $SSH_KEY_FOLDER/id_rsa:/root/.ssh/id_rsa \
+        docker run --rm --platform=$platform -v $SSH_KEY_FOLDER/id_rsa:/root/.ssh/id_rsa \
             -v $SSH_KEY_FOLDER/id_rsa.pub:/root/.ssh/id_rsa.pub \
             -v $SSH_KEY_FOLDER/known_hosts:/root/.ssh/known_hosts \
-            $LOCAL_REGISTRY/base-test:git-latest git clone https://github.com/aws/eks-distro.git
+            $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:git-latest git clone https://github.com/aws/eks-distro.git
 
         # use lib git to clone private and public repo
-        if docker run --rm --platform=$platform --pull=always -v $SSH_KEY_FOLDER/id_rsa:/root/.ssh/id_rsa \
+        if docker run --rm --platform=$platform -v $SSH_KEY_FOLDER/id_rsa:/root/.ssh/id_rsa \
             -v $SSH_KEY_FOLDER/id_rsa.pub:/root/.ssh/id_rsa.pub \
             -v $SSH_KEY_FOLDER/known_hosts:/root/.ssh/known_hosts \
-            -e PRIVATE_REPO=$PRIVATE_REPO $LOCAL_REGISTRY/base-test:git-latest check-git | grep -v 'Successfully cloned!'; then
+            -e PRIVATE_REPO=$PRIVATE_REPO $LOCAL_REGISTRY/eks-distro-minimal-images-base-test:git-latest check-git | grep -v 'Successfully cloned!'; then
             echo "git issue!"
             exit 1
         fi
@@ -219,7 +263,9 @@ function check_base-csi() {
 
  check_base-docker-client() {
     for platform in ${PLATFORMS//,/ }; do
-        if ! docker run --rm --platform=$platform --pull=always -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_REPO/eks-distro-minimal-base-docker-client:$IMAGE_TAG docker info; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-docker-client:$IMAGE_TAG
+
+        if ! docker run --rm --platform=$platform -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_REPO/eks-distro-minimal-base-docker-client:$IMAGE_TAG docker info; then
             echo "docker client issue!"
             exit 1
         fi
@@ -228,7 +274,9 @@ function check_base-csi() {
 
  check_base-haproxy() {
     for platform in ${PLATFORMS//,/ }; do
-        if ! docker run --rm --platform=$platform --pull=always $IMAGE_REPO/eks-distro-minimal-base-haproxy:$IMAGE_TAG haproxy -v; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-haproxy:$IMAGE_TAG
+
+        if ! docker run --rm --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-haproxy:$IMAGE_TAG haproxy -v; then
             echo "haproxy issue!"
             exit 1
         fi
@@ -237,7 +285,9 @@ function check_base-csi() {
 
  check_base-nginx() {
     for platform in ${PLATFORMS//,/ }; do
-        if ! docker run --rm --platform=$platform --pull=always $IMAGE_REPO/eks-distro-minimal-base-nginx:$IMAGE_TAG nginx -v; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-nginx:$IMAGE_TAG
+
+        if ! docker run --rm --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-nginx:$IMAGE_TAG nginx -v; then
             echo "nginx issue!"
             exit 1
         fi
@@ -246,7 +296,9 @@ function check_base-csi() {
 
  check_base-kind() {
     for platform in ${PLATFORMS//,/ }; do
-        if ! docker run --rm --platform=$platform --pull=always $IMAGE_REPO/eks-distro-minimal-base-kind:$IMAGE_TAG ctr -v; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-kind:$IMAGE_TAG
+
+        if ! docker run --rm --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-kind:$IMAGE_TAG ctr -v; then
             echo "kind issue!"
             exit 1
         fi
@@ -255,7 +307,9 @@ function check_base-csi() {
 
  check_base-nsenter() {
     for platform in ${PLATFORMS//,/ }; do
-        if docker run --rm --platform=$platform --pull=always $IMAGE_REPO/eks-distro-minimal-base-nsenter:$IMAGE_TAG nsenter --version | grep -v 'nsenter from util-linux'; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/eks-distro-minimal-base-nsenter:$IMAGE_TAG
+
+        if docker run --rm --platform=$platform  $IMAGE_REPO/eks-distro-minimal-base-nsenter:$IMAGE_TAG nsenter --version | grep -v 'nsenter from util-linux'; then
             echo "nsenter issue!"
             exit 1
         fi
@@ -269,37 +323,39 @@ function check_base-csi() {
     # Cases based on distroless's test cases
     # https://github.com/GoogleContainerTools/distroless/blob/main/experimental/python3/testdata/python3.yaml
     for platform in ${PLATFORMS//,/ }; do
-        if docker run --rm --platform=$platform --pull=always $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "print('Hello World')" | grep -v 'Hello World'; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG
+
+        if docker run --rm --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "print('Hello World')" | grep -v 'Hello World'; then
             echo "python3 issue!"
             exit 1
         fi
     
-        if docker run --rm --platform=$platform --pull=always $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "import subprocess, sys; subprocess.check_call(sys.executable + ' --version', shell=True)" | grep -v 'Python 3'; then
+        if docker run --rm --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "import subprocess, sys; subprocess.check_call(sys.executable + ' --version', shell=True)" | grep -v 'Python 3'; then
             echo "python3 issue!"
             exit 1
         fi
 
-        if ! docker run --rm --platform=$platform --pull=always $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "import ctypes.util; ctypes.CDLL(ctypes.util.find_library('rt')).timer_create"; then
+        if ! docker run --rm --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "import ctypes.util; ctypes.CDLL(ctypes.util.find_library('rt')).timer_create"; then
             echo "python3 issue!"
             exit 1
         fi
 
-        if ! docker run --rm --platform=$platform --pull=always $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "import distutils.dist"; then
+        if ! docker run --rm --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "import distutils.dist"; then
             echo "python3 issue!"
             exit 1
         fi
 
-        if docker run --rm --platform=$platform --pull=always $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "open(u'h\\xe9llo', 'w'); import sys; print(sys.getfilesystemencoding())" | grep -v 'utf-8'; then
+        if docker run --rm --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "open(u'h\\xe9llo', 'w'); import sys; print(sys.getfilesystemencoding())" | grep -v 'utf-8'; then
             echo "python3 issue!"
             exit 1
         fi
 
-        if docker run --rm --platform=$platform --pull=always -v $SCRIPT_ROOT/python3-test.py:/eks-d-python-validate.py $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 /eks-d-python-validate.py | grep -v 'FINISHED ENTIRE SCRIPT'; then
+        if docker run --rm --platform=$platform -v $SCRIPT_ROOT/python3-test.py:/eks-d-python-validate.py $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 /eks-d-python-validate.py | grep -v 'FINISHED ENTIRE SCRIPT'; then
             echo "python3 issue!"
             exit 1
         fi
 
-        if ! docker run --rm --platform=$platform --pull=always $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "import ssl; print(ssl.OPENSSL_VERSION)"; then
+        if ! docker run --rm --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG /usr/bin/python3 -c "import ssl; print(ssl.OPENSSL_VERSION)"; then
             echo "python3 issue!"
             exit 1
         fi
@@ -313,7 +369,9 @@ check_base-python-3.9() {
 check_base-compiler-base() {
     local -r image_component="${1:-compiler-base}"
     for platform in ${PLATFORMS//,/ }; do
-        if ! docker run --rm --platform=$platform --pull=always $IMAGE_REPO/$image_component:$IMAGE_TAG curl --version; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG
+
+        if ! docker run --rm --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG curl --version; then
             echo "compiler-base issue!"
             exit 1
         fi
@@ -323,7 +381,9 @@ check_base-compiler-base() {
 check_base-compiler-yum() {
     local -r image_component="${1:-compiler-base}"
     for platform in ${PLATFORMS//,/ }; do
-        if ! docker run --rm --platform=$platform --pull=always $IMAGE_REPO/$image_component:$IMAGE_TAG yum --version; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG
+
+        if ! docker run --rm --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG yum --version; then
             echo "compiler-base issue!"
             exit 1
         fi
@@ -333,7 +393,9 @@ check_base-compiler-yum() {
 check_base-compiler-gcc() {
     local -r image_component="${1:-compiler-base}"
     for platform in ${PLATFORMS//,/ }; do
-        if ! docker run --rm --platform=$platform --pull=always $IMAGE_REPO/$image_component:$IMAGE_TAG gcc --version; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG
+
+        if ! docker run --rm --platform=$platform $IMAGE_REPO/$image_component:$IMAGE_TAG gcc --version; then
             echo "compiler-base issue!"
             exit 1
         fi
@@ -347,7 +409,9 @@ check_base-golang-compiler() {
     check_base-compiler-$2 golang
     
     for platform in ${PLATFORMS//,/ }; do
-        if docker run --rm --platform=$platform --pull=always $IMAGE_REPO/golang:$IMAGE_TAG go version | grep -v $version; then
+        build::docker::retry_pull --platform=$platform $IMAGE_REPO/golang:$IMAGE_TAG
+
+        if docker run --rm --platform=$platform $IMAGE_REPO/golang:$IMAGE_TAG go version | grep -v $version; then
             echo "golang issue!"
             exit 1
         fi
