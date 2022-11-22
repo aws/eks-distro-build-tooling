c76fa35a6875d6e3e26e8e05343792862a9412c8
Jackson West <jgw@amazon.com>
2022-11-22 14:40:49 -0600

    Update check_update.sh

--- eks-distro-base/check_update.sh
+++ eks-distro-base/check_update.sh
@@ -44,7 +44,7 @@ COPY --from=base_image /etc/yum.repos.d /etc/yum.repos.d
 RUN set -x && \
     # yum will think there are updates avail for the golang packages which we are managing
     # seperately. Remove them to ensure they do not trigger builds
-    rpm -e --justdb --nodeps $(rpm -qav | grep golang) || true && \
+    rpm -e --justdb --nodeps \$(rpm -qav | grep golang) || true && \
     if grep -q "2022" "/etc/os-release"; then \
         yum check-update --security --releasever=latest  > ./check_update_output; echo \$? > ./return_value; \
     else \
