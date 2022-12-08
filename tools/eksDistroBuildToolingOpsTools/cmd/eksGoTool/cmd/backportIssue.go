26f0f0ae6a34bc325b78691e263c582bf798c34f
Cameron Rozean <rcrozean@amazon.com>
2022-12-08 13:11:39 -0800

    update logging and global var organization

--- tools/eksDistroBuildToolingOpsTools/cmd/eksGoTool/cmd/backportIssue.go
+++ tools/eksDistroBuildToolingOpsTools/cmd/eksGoTool/cmd/backportIssue.go
@@ -6,14 +6,16 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/github"
-	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/issueManager"
-	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/repoContentManager"
-	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/retrier"
-
 	gogithub "github.com/google/go-github/v48/github"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/constants"
+	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/github"
+	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/issueManager"
+	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/logger"
+	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/repoManager"
+	"github.com/aws/eks-distro-build-tooling/tools/eksDistroBuildToolingOpsTools/pkg/retrier"
 )
 
 const (
@@ -28,104 +30,17 @@ const (
 	eksGoSupportedVersionsPath = "projects/golang/go/MAINTAINED_EOL_VERSIONS"
 )
 
-var (
-	toplevelIssueId  int
+type backportIssueOptions struct {
 	backportVersions []string
+	toplevelIssueId  int
+}
 
-	backportIssueCmd = &cobra.Command{
-		Use:   "issue",
-		Short: "Opens backport issues for top level github issue",
-		Long:  `Opens issues to backport top level issue to EKS-Distro supported versions of Golang`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-
-			label := []string{"golang", "security"}
-			assignee := "rcrozean"
-			issueState := "open"
-
-			retrier := retrier.New(time.Second*380, retrier.WithBackoffFactor(1.5), retrier.WithMaxRetries(15, time.Second*30))
-
-			token, err := github.GetGithubToken()
-			if err != nil {
-				return fmt.Errorf("getting Github PAT from environment at variable %s: %v", github.PersonalAccessTokenEnvVar, err)
-			}
-			githubClient, err := github.NewClient(cmd.Context(), token)
-			if err != nil {
-				return fmt.Errorf("setting up Github client: %v", err)
-			}
-
-			if backportVersions[0] == "" {
-				// set up Repo Content Creator handler
-				repoContentManagerOpts := &repoContentManager.Opts{
-					SourceOwner: "rcrozean",
-					SourceRepo:  "eks-distro-build-tooling",
-				}
-				rm := repoContentManager.New(retrier, githubClient, repoContentManagerOpts)
-
-				gfOpts := &repoContentManager.GetFileOpts{
-					Owner: "aws",
-					Repo:  "eks-distro-build-tooling",
-					Path:  eksGoSupportedVersionsPath,
-					Ref:   nil,
-				}
-
-				test, err := rm.GetFile(cmd.Context(), gfOpts)
-				if err != nil {
-					return fmt.Errorf("getting file at %s: %v", gfOpts.Path, err)
-				}
-				testContent, err := test.GetContent()
-				if err != nil {
-					return fmt.Errorf("getting file content from %v: %v", test.Name, err)
-				}
-
-				backportVersions = strings.Split(testContent, "\n")
-			}
-
-			// set up Issue Creator handler
-			issueManagerOpts := &issueManager.Opts{
-				SourceOwner: "rcrozean",
-				SourceRepo:  "eks-distro-build-tooling",
-			}
-			im := issueManager.New(retrier, githubClient, issueManagerOpts)
-
-			giOpts := &issueManager.GetIssueOpts{
-				Owner: "aws",
-				Repo:  "eks-distro-build-tooling",
-				Issue: toplevelIssueId,
-			}
-
-			toplevelIssue, err := im.GetIssue(cmd.Context(), giOpts)
-			if err != nil {
-				return fmt.Errorf("getting toplevel issue %v from %v: %v", giOpts.Issue, giOpts.Repo, err)
-			}
-
-			for _, ver := range backportVersions {
-				if ver != "" {
-					issueOpts := &issueManager.CreateIssueOpts{
-						Title:    GenerateBackportIssueTitle(toplevelIssue, ver),
-						Body:     GenerateBackportIssueBody(toplevelIssue, ver),
-						Labels:   &label,
-						Assignee: &assignee,
-						State:    &issueState,
-					}
-
-					if !dryRun {
-						_, err := im.CreateIssue(cmd.Context(), issueOpts)
-						if err != nil {
-							return fmt.Errorf("creating issue: %v", err)
-						}
-					}
-				}
-			}
-
-			return nil
-		},
-	}
-)
+var bpOptions = &backportIssueOptions{}
 
 func init() {
 	backportCmd.AddCommand(backportIssueCmd)
-	backportIssueCmd.Flags().IntVarP(&toplevelIssueId, toplevelIssueIdFlag, "i", 0, "Upstream Issue ID e.g. ")
-	backportIssueCmd.Flags().StringSliceVarP(&backportVersions, backportVersionFlag, "b", []string{""}, "to specify versions to backport use this flag. Multiple versions can be specified separated by commas. e.g. <ver>,<ver>,<ver>")
+	backportIssueCmd.Flags().IntVarP(&bpOptions.toplevelIssueId, toplevelIssueIdFlag, "i", 0, "Issue ID to be backported e.g. 254")
+	backportIssueCmd.Flags().StringSliceVarP(&bpOptions.backportVersions, backportVersionFlag, "b", nil, "to specify versions to backport use this flag. Multiple versions can be specified separated by commas. e.g. <ver>,<ver>,<ver>")
 
 	requiredFlags := []string{
 		toplevelIssueIdFlag,
@@ -137,14 +52,99 @@ func init() {
 	}
 }
 
+var backportIssueCmd = &cobra.Command{
+	Use:   "issue",
+	Short: "Opens backport issues for top level github issue",
+	Long:  "Opens issues to backport top level issue to EKS-Distro supported versions of Golang",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		label := []string{"golang", "security"}
+		issueState := "open"
+
+		retrier := retrier.New(time.Second*380, retrier.WithBackoffFactor(1.5), retrier.WithMaxRetries(15, time.Second*30))
+
+		token, err := github.GetGithubToken()
+		if err != nil {
+			return fmt.Errorf("getting Github PAT from environment at variable %s: %v", github.PersonalAccessTokenEnvVar, err)
+		}
+		githubClient, err := github.NewClient(cmd.Context(), token)
+		if err != nil {
+			return fmt.Errorf("setting up Github client: %v", err)
+		}
+
+		if bpOptions.backportVersions == nil { //When no option was passed to the flag
+			// set up Repo Content Creator handler
+			repoManagerOpts := &repoManager.Opts{
+				SourceOwner: constants.Aws,
+				SourceRepo:  constants.EksdBuildTooling,
+			}
+			rm := repoManager.New(retrier, githubClient, repoManagerOpts)
+
+			gfOpts := &repoManager.GetFileOpts{
+				Owner: "aws",
+				Repo:  "eks-distro-build-tooling",
+				Path:  eksGoSupportedVersionsPath,
+				Ref:   nil,
+			}
+
+			test, err := rm.GetFile(cmd.Context(), gfOpts)
+			if err != nil {
+				return fmt.Errorf("getting file at %s: %v", gfOpts.Path, err)
+			}
+			testContent, err := test.GetContent()
+			if err != nil {
+				return fmt.Errorf("getting file content from %v: %v", test.Name, err)
+			}
+
+			bpOptions.backportVersions = strings.Split(testContent, "\n")
+		}
+
+		// set up Issue Creator handler
+		issueManagerOpts := &issueManager.Opts{
+			SourceOwner: constants.Aws,
+			SourceRepo:  constants.EksdBuildTooling,
+		}
+		im := issueManager.New(retrier, githubClient, issueManagerOpts)
+
+		giOpts := &issueManager.GetIssueOpts{
+			Owner: constants.Aws,
+			Repo:  constants.EksdBuildTooling,
+			Issue: bpOptions.toplevelIssueId,
+		}
+
+		toplevelIssue, err := im.GetIssue(cmd.Context(), giOpts)
+		if err != nil {
+			return fmt.Errorf("getting toplevel issue %v from %v: %v", giOpts.Issue, giOpts.Repo, err)
+		}
+
+		for _, ver := range bpOptions.backportVersions {
+			if ver != "" {
+				issueOpts := &issueManager.CreateIssueOpts{
+					Title:    GenerateBackportIssueTitle(toplevelIssue, ver),
+					Body:     GenerateBackportIssueBody(toplevelIssue, ver),
+					Labels:   &label,
+					Assignee: nil,
+					State:    &issueState,
+				}
+
+				_, err := im.CreateIssue(cmd.Context(), issueOpts)
+				if err != nil {
+					return fmt.Errorf("creating issue: %v", err)
+				}
+			}
+		}
+
+		return nil
+	},
+}
+
 func GenerateBackportIssueBody(ui *gogithub.Issue, ver string) *string {
 	b := strings.Builder{}
 
-	b.WriteString(fmt.Sprintf("A backport of %v to EKS Go %v was requested by %v", *ui.HTMLURL, ver, viper.GetString(authorNameFlag)))
+	b.WriteString(fmt.Sprintf("A backport of issue %v to EKS Go %v was requested by %v\n", *ui.HTMLURL, ver, viper.GetString(authorNameFlag)))
+	b.WriteString(fmt.Sprintf("%v", *ui.Body))
 	bs := b.String()
-	if dryRun {
-		fmt.Printf("Created Issues Body: `%s`\n", bs)
-	}
+	logger.V(4).Info("Created Issues Body: `%s`\n", bs)
 	return &bs
 }
 
@@ -156,8 +156,6 @@ func GenerateBackportIssueTitle(ui *gogithub.Issue, ver string) *string {
 	}
 	ts := t.String()
 
-	if dryRun {
-		fmt.Printf("Created Issues Title: `%s`\n", ts)
-	}
+	logger.V(4).Info("Created Issues Title: `%s`\n", ts)
 	return &ts
 }
--- tools/eksDistroBuildToolingOpsTools/pkg/repoManager/repoContentManager.go
+++ tools/eksDistroBuildToolingOpsTools/pkg/repoManager/repoContentManager.go
@@ -1,4 +1,4 @@
-package repoContentManager
+package repoManager
 
 import (
 	"context"
