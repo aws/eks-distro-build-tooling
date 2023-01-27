f685f30259692e70900c6b00fc2864fe7fa16357
Sajia Zafreen <szafreen@amazon.com>
2023-01-27 13:04:48 -0800

    test 1

--- tools/eksDistroBuildToolingOpsTools/pkg/constants/constants.go
+++ tools/eksDistroBuildToolingOpsTools/pkg/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	Aws                        = "aws"
+	Aws                        = "aws" 
 	EksdBuildTooling           = "eks-distro-build-tooling"
 	EksGoSupportedVersionsPath = "projects/golang/go/MAINTAINED_EOL_VERSIONS"
 )
