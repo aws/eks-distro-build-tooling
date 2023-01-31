6f146eb879b1c148c6d668536ff8a72e6659d605
Sajia Zafreen <szafreen@amazon.com>
2023-01-31 15:52:19 -0800

    edit spacing

--- builder-base/scripts/download_golang.sh
+++ builder-base/scripts/download_golang.sh
@@ -31,7 +31,7 @@ function build::go::download(){
     # the function again with the specific parameter.
     local version=${1%-*}
     local outputDir=${2}
-    local arch=${1}
+    local arch=${3}
 
     for artifact in golang golang-bin; do
         local filename="$outputDir/$artifact-$version-$RELEASE_NUMBER.amzn2.eks.$arch.rpm"
@@ -42,9 +42,9 @@ function build::go::download(){
         curl -sSL --retry 5 https://distro.eks.amazonaws.com/golang-go$version/releases/$RELEASE_NUMBER/RPMS/$arch/$artifact-$version-$RELEASE_NUMBER.amzn2.eks.$arch.rpm.sha256 -o $filename.sha256
 
         if [[ $(sha256sum ${filename} | cut -d' ' -f1) != $(cut -d' ' -f1 "${filename}.sha256") ]] ; then 
-			echo "Checksum doesn't match!"
+            echo "Checksum doesn't match!"
             exit 1
-		fi
+        fi
     done
 
     if [ $arch == 'x86_64' ]; then
@@ -56,10 +56,9 @@ function build::go::download(){
         curl -sSL --retry 5 https://distro.eks.amazonaws.com/golang-go$version/releases/$RELEASE_NUMBER/RPMS/$arch/golang-race-$version-$RELEASE_NUMBER.amzn2.eks.$arch.rpm.sha256 -o $filename.sha256
 
         if [[ $(sha256sum ${filename} | cut -d' ' -f1) != $(cut -d' ' -f1 "${filename}.sha256") ]] ; then 
-			echo "Checksum doesn't match!"
+            echo "Checksum doesn't match!"
             exit 1
-		fi
-
+        fi
     fi
 
     for artifact in golang-docs golang-misc golang-tests golang-src; do
@@ -71,9 +70,9 @@ function build::go::download(){
         curl -sSL --retry 5 https://distro.eks.amazonaws.com/golang-go$version/releases/$RELEASE_NUMBER/RPMS/noarch/$artifact-$version-$RELEASE_NUMBER.amzn2.eks.noarch.rpm.sha256 -o $filename.sha256
 
         if [[ $(sha256sum ${filename} | cut -d' ' -f1) != $(cut -d' ' -f1 "${filename}.sha256") ]] ; then 
-			echo "Checksum doesn't match!"
+            echo "Checksum doesn't match!"
             exit 1
-		fi
+        fi
     done
 }
 
