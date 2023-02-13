2caf1a49994d56f6e6eb336ca27c103ebc2984be
Cameron Rozean <rcrozean@amazon.com>
2023-02-13 14:10:13 -0800

    change author to login name

--- tools/eksDistroBuildToolingOpsTools/cmd/eksDistroOpsProwPlugin/server.go
+++ tools/eksDistroBuildToolingOpsTools/cmd/eksDistroOpsProwPlugin/server.go
@@ -125,7 +125,7 @@ func (s *Server) handleIssue(l *logrus.Entry, ie github.IssueEvent) error {
 	org := ie.Repo.Owner.Login
 	repo := ie.Repo.Name
 	num := ie.Issue.Number
-	auth := ie.Sender.Name
+	auth := ie.Sender.Login
 	title := ie.Issue.Title
 	body := ie.Issue.Body
 
