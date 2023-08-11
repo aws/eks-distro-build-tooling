b9e987cbc8c00a1df6f67a5de64cbbbcc0704b67
Kirsten Schumy <ksschumy@amazon.com>
2023-08-11 10:16:16 -0700

    Added check for files exists (#1061)
    * Added check for files exists
    
    * Removed unneeded check

--- eks-distro-base/update_base_image_other_repos.sh
+++ eks-distro-base/update_base_image_other_repos.sh
@@ -31,7 +31,7 @@ EXTRA_PR_BODY=""
 
 REPOS=(eks-distro eks-anywhere-build-tooling eks-anywhere)
 for repo in "${REPOS[@]}"; do
-    ${SCRIPT_ROOT}/../pr-scripts/update_local_branch.sh "$repo"    
+    ${SCRIPT_ROOT}/../pr-scripts/update_local_branch.sh "$repo"
 done
 while IFS=, read -r key
 do
@@ -55,7 +55,7 @@ do
             ${SCRIPT_ROOT}/../pr-scripts/update_image_tag.sh "$repo" '.*' $IMAGE_TAG $BASE_IMAGE_TAG_FILE
 
             if [ "$(git -C ${OTHER_CLONE_ROOT}/${ORIGIN_ORG}/${repo} status --porcelain -- $BASE_IMAGE_TAG_FILE | wc -l)" -gt 0 ]; then
-                UPDATE_PACKAGES="$(cat ${SCRIPT_ROOT}/../eks-distro-base-updates/${key#al}/update_packages-${image})"
+                UPDATE_PACKAGES="$(cat "${SCRIPT_ROOT}/../eks-distro-base-updates/${key#al}/update_packages-${image}")"
                 if [ "$UPDATE_PACKAGES" != "" ]; then
                     EXTRA_PR_BODY+="\n${BASE_IMAGE_TAG_FILE}\nThe following yum packages were updated:\n\`\`\`bash\n${UPDATE_PACKAGES}\n\`\`\`\n"
                 fi
