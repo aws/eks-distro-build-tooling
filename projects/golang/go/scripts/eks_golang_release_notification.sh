494b47d21098f4f1c2577d92e1a10c1c17f30a0e
Kirsten Schumy <ksschumy@amazon.com>
2023-08-22 12:32:25 -0700

    Potential golang sns fix (#1119)

--- projects/golang/go/scripts/eks_golang_release_notification.sh
+++ projects/golang/go/scripts/eks_golang_release_notification.sh
@@ -12,7 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+set -e
+set -x
 
 if [ "$AWS_ROLE_ARN" == "" ]; then
     echo "Empty AWS_ROLE_ARN"
@@ -51,7 +52,8 @@ source_profile=default
 EOF
 export AWS_CONFIG_FILE=$(pwd)/awscliconfig
 export AWS_PROFILE=artifacts-push
-unset AWS_ROLE_ARN AWS_WEB_IDENTITY_TOKEN_FILE
+cat awscliconfig
+# unset AWS_ROLE_ARN AWS_WEB_IDENTITY_TOKEN_FILE
 
 GOLANG_TRACKING_TAG="$(cat $BASE_DIRECTORY/projects/golang/go/$GO_SOURCE_VERSION/GIT_TAG)"
 SNS_MESSAGE="eks_golang_release: $(cat $BASE_DIRECTORY/projects/golang/go/$GO_SOURCE_VERSION/RELEASE)
