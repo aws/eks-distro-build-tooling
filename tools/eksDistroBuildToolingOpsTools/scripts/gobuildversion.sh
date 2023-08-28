ba44f0196b15d5d76dbd4fbb800bdf4befc97e5b
Cameron Rozean <rcrozean@amazon.com>
2023-08-28 12:20:32 -0700

    set go build version in builderbase

---
+++ tools/eksDistroBuildToolingOpsTools/scripts/gobuildversion.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+if [ -n "${OUTPUT_DEBUG_LOG:-}" ]; then
+    set -x
+fi
+
+function build::common::get_go_path() {
+  local -r version=$1
+
+  # This is the path where the specific go binary versions reside in our builder-base image
+  local -r gorootbinarypath="/go/go${version}/bin"
+  # This is the path that will most likely be correct if running locally
+  local -r gopathbinarypath="$GOPATH/go${version}/bin"
+  if [ -d "$gorootbinarypath" ]; then
+    echo $gorootbinarypath
+  elif [ -d "$gopathbinarypath" ]; then
+    echo $gopathbinarypath
+  else
+    # not in builder-base, probably running in dev environment
+    # return default go installation
+    local -r which_go=$(which go)
+    echo "$(dirname $which_go)"
+  fi
+}
+
+function build::common::use_go_version() {
+  local -r version=$1
+
+  if (( "${version#*.}" < 16 )); then
+    echo "Building with GO version $version is no longer supported!  Please update the build to use a newer version."
+    exit 1
+  fi
+
+  local -r gobinarypath=$(build::common::get_go_path $version)
+  echo "Adding $gobinarypath to PATH"
+  # Adding to the beginning of PATH to allow for builds on specific version if it exists
+  export PATH=${gobinarypath}:$PATH
+  export GOCACHE=$(go env GOCACHE)/$version
+}
+
+# Use a seperate build cache for each project/version to ensure there are no
+# shared bits which can mess up the final checksum calculation
+# this is mostly needed for create checksums locally since in the builds
+# different versions of the same project are not built in the same container
+function build::common::set_go_cache() {
+  local -r project=$1
+  local -r git_tag=$2
+  export GOCACHE=$(go env GOCACHE)/$project/$git_tag
+}
