7f6f06993779aed515d413d1c3b0b1c05202372f
Cameron Rozean <rcrozean@amazon.com>
2023-12-12 11:01:32 -0800

    update sns message for clarity

--- projects/golang/go/scripts/eks_golang_release_notification.sh
+++ projects/golang/go/scripts/eks_golang_release_notification.sh
@@ -13,31 +13,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 if [ "$AWS_ROLE_ARN" == "" ]; then
-    echo "Empty AWS_ROLE_ARN"
-    exit 1
+	echo "Empty AWS_ROLE_ARN"
+	exit 1
 fi
 
 if [ "$ARTIFACT_DEPLOYMENT_ROLE_ARN" == "" ]; then
-    echo "Empty ARTIFACT_DEPLOYMENT_ROLE_ARN"
-    exit 1
+	echo "Empty ARTIFACT_DEPLOYMENT_ROLE_ARN"
+	exit 1
 fi
 
 if [ "$SNS_TOPIC_ARN" == "" ]; then
-    echo "Empty SNS_TOPIC_ARN"
-    exit 1
+	echo "Empty SNS_TOPIC_ARN"
+	exit 1
 fi
 
 if [ "$GO_SOURCE_VERSION" == "" ]; then
-    echo "Empty GO_SOURCE_VERSION"
-    exit 1
+	echo "Empty GO_SOURCE_VERSION"
+	exit 1
 fi
 
 BASE_DIRECTORY=$(git rev-parse --show-toplevel)
 cd $BASE_DIRECTORY || exit
 
-cat << EOF > awscliconfig
+cat <<EOF >awscliconfig
 [default]
 output=json
 region=${AWS_REGION:-${AWS_DEFAULT_REGION:-us-west-2}}
@@ -56,21 +55,24 @@ unset AWS_ROLE_ARN AWS_WEB_IDENTITY_TOKEN_FILE
 GOLANG_TRACKING_TAG="$(cat $BASE_DIRECTORY/projects/golang/go/$GO_SOURCE_VERSION/GIT_TAG)"
 SNS_MESSAGE="eks_golang_release: $(cat $BASE_DIRECTORY/projects/golang/go/$GO_SOURCE_VERSION/RELEASE)
 golang_tracking_tag: $GOLANG_TRACKING_TAG
-golang_tracking_version: ${GOLANG_TRACKING_TAG:2}" # removes "go" at front
+golang_tracking_version: ${GOLANG_TRACKING_TAG:2}
+metadata:
+  summary: This is the EKS Go source release, not the minimal base or debian images. See the link in the github tag for artifact source locations.
+  github: https://github.com/aws/$BASE_DIRECTORY/projects/golang/go/$GO_SOURCE_VERSION"
 
 SNS_MESSAGE_ID=$(
-  aws sns publish \
-    --profile "artifacts-push" \
-    --topic-arn "$SNS_TOPIC_ARN" \
-    --subject "New Release of EKS Golang v$GO_SOURCE_VERSION" \
-    --message "$SNS_MESSAGE"\
-    --query "MessageId" --output text
+	aws sns publish \
+		--profile "artifacts-push" \
+		--topic-arn "$SNS_TOPIC_ARN" \
+		--subject "New Release of EKS Golang v$GO_SOURCE_VERSION" \
+		--message "$SNS_MESSAGE" \
+		--query "MessageId" --output text
 )
 
 if [ "$SNS_MESSAGE_ID" ]; then
-  echo -e "\nEKS Golang release notification published with SNS MessageId $SNS_MESSAGE_ID"
+	echo -e "\nEKS Golang release notification published with SNS MessageId $SNS_MESSAGE_ID"
 else
-  echo -e "Received unexpected response while publishing to EKS Golang release SNS topic $SNS_TOPIC_ARN. \
+	echo -e "Received unexpected response while publishing to EKS Golang release SNS topic $SNS_TOPIC_ARN. \
 An error may have occurred, and the notification may not have been published"
-  exit 1
+	exit 1
 fi
