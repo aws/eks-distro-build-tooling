607942e9b9e13df1e5f8723226fb00eceed778d4
Cameron Rozean <rcrozean@amazon.com>
2024-07-12 13:16:45 -0700

    Add script to check and update builder base for new Golang version (#1449)
    * add script to pull upstream patch versions and update versions.yaml if they don't exist in file
    
    * add function to null golang images on updates to versions.yaml
    
    * finalize script to update golang minimal image versions and values for all patch and minor version updates
    
    * move golang versions to yaml, use yaml in Makefile, autoupdate yaml with upstream new versions
    
    * fix path
    
    * eval variable
    
    * readd vars to make
    
    * correct vars to make
    
    * remove golang versions from Makefile
    
    * fix new versions path
    
    * add golang versions file back

---
+++ builder-base/scripts/check_upstream_golang.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+set -o pipefail
+set -x
+
+GO_PREFIX="go"
+SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+VERSIONS_YAML="${SCRIPT_ROOT}/../versions.yaml"
+TAG_FILE="${SCRIPT_ROOT}/../../EKS_DISTRO_TAG_FILE.yaml"
+
+function update::go::version {
+  local -r version=$1
+  local -r majorversion=$(if [[ $(echo "$version" | awk -F'.' '{print NF}') -ge 3 ]]; then echo ${version%.*}; else echo ${version%-*}; fi)
+
+  local -r cur_builder_base_version=$(cat "${VERSIONS_YAML}" | grep -E "^GOLANG_VERSION_${majorversion//./}")
+
+  gsed -i "s/${cur_builder_base_version}/GOLANG_VERSION_${majorversion//./}: ${version}-0/g" "${VERSIONS_YAML}"
+}
+
+function add::go::version {
+  local -r version=$1
+  local -r majorversion=$(if [[ $(echo "$version" | awk -F'.' '{print NF}') -ge 3 ]]; then echo ${version%.*}; else echo ${version%-*}; fi)
+
+  echo "GOLANG_VERSION_${majorversion//./}: $version-0" >>$VERSIONS_YAML
+}
+
+# Using YQ allows us to modify the existing tag or add the correct tag if it doesn't exist
+function modify::go::minimal_image {
+  local -r version=$1
+  local -r majorversion=$(if [[ $(echo "$version" | awk -F'.' '{print NF}') -ge 3 ]]; then echo ${version%.*}; else echo ${version%-*}; fi)
+
+  # AL2 updates
+  AL2BASE=".al2.[\"eks-distro-minimal-base-golang-compiler-${majorversion}-base\"]" yq 'eval(strenv(AL2BASE)) = null' -i $TAG_FILE
+  AL2YUM=".al2.[\"eks-distro-minimal-base-golang-compiler-${majorversion}-yum\"]" yq 'eval(strenv(AL2YUM)) = null' -i $TAG_FILE
+  AL2GCC=".al2.[\"eks-distro-minimal-base-golang-compiler-${majorversion}-gcc\"]" yq 'eval(strenv(AL2GCC)) = null' -i $TAG_FILE
+
+  # AL2023 updates
+  AL2023BASE=".al2023.[\"eks-distro-minimal-base-golang-compiler-${majorversion}-base\"]" yq 'eval(strenv(AL2023BASE)) = null' -i $TAG_FILE
+  AL2023YUM=".al2023.[\"eks-distro-minimal-base-golang-compiler-${majorversion}-yum\"]" yq 'eval(strenv(AL2023YUM)) = null' -i $TAG_FILE
+  AL2023GCC=".al2023.[\"eks-distro-minimal-base-golang-compiler-${majorversion}-gcc\"]" yq 'eval(strenv(AL2023GCC)) = null' -i $TAG_FILE
+}
+
+# curl go.dev for the supported versions of go
+ACTIVE_VERSIONS=$(curl https://go.dev/dl/?mode=json | jq -r '.[].version' | sed -e "s/^$GO_PREFIX//" | sort)
+
+for version in ${ACTIVE_VERSIONS}; do
+  # pull golang versions in the versions.yaml
+  MAJORVERSION=$(if [[ $(echo "$version" | awk -F'.' '{print NF}') -ge 3 ]]; then echo ${version%.*}; else echo ${version%-*}; fi)
+  BUILDER_BASE_GO_VERSION=$(cat "${VERSIONS_YAML}" | grep -E "^GOLANG_VERSION_${MAJORVERSION//./}") || ""
+  # check builder-base versions for the upstream version of golang
+  # if the version doesn't exist in the builder base update the versions yaml.
+  if [[ -n $BUILDER_BASE_GO_VERSION && ! $BUILDER_BASE_GO_VERSION =~ $version ]]; then
+    update::go::version $version
+    modify::go::minimal_image $version
+  elif [[ -z $BUILDER_BASE_GO_VERSION ]]; then
+    add::go::version $version
+    modify::go::minimal_image $version
+  fi
+done
