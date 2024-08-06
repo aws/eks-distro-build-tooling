37b340b9bd37185fe620fb4ab38ad8b46246c0e4
Cameron Rozean <rcrozean@amazon.com>
2024-08-06 20:27:26 +0000

    cp files from main repo to fork

--- pr-scripts/create_pr.sh
+++ pr-scripts/create_pr.sh
@@ -119,7 +119,12 @@ if [[ $JOB_NAME =~ $MINIMAL_IMAGE_REBUILD_PJ_NAME ]]; then
   git add ../EKS_DISTRO_TAG_FILE.yaml
 fi
 if [[ $JOB_NAME =~ $GOLANG_RELEASE_PERIODIC ]]; then
-  git diff --name-only
+  # sync expected files changed during the previous make calls in aws/eks-distro-build-tooling to pr-bot fork
+  cp ${SCRIPT_ROOT}/../EKS_DISTRO_TAG_FILE.yaml ./EKS_DISTRO_TAG_FILE.yaml
+  cp ${SCRIPT_ROOT}/../eks-distro-base/golang_versions.yaml ./eks-distro-base/golang_versions.yaml
+  cp ${SCRIPT_ROOT}/../builder-base/versions.yaml ./builder-base/versions.yaml
+  cp ${SCRIPT_ROOT}/../builder-base/checksums/go* ./builder-base/checksums/go*
+  cp ${SCRIPT_ROOT}/../eks-distro-base/make-tests/expected/*golang* ./eks-distro-base/make-tests/expected/*golang*
   git add ./EKS_DISTRO_TAG_FILE.yaml
   git add ./eks-distro-base/golang_versions.yaml
   git add ./builder-base/versions.yaml
