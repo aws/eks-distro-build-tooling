c05a769cfe2e779e9cfdbace1910642710a68cfb
Cameron Rozean <rcrozean@amazon.com>
2024-08-06 15:33:12 -0700

    update verbosity for cp in golang periodic

--- pr-scripts/create_pr.sh
+++ pr-scripts/create_pr.sh
@@ -123,8 +123,8 @@ if [[ $JOB_NAME =~ $GOLANG_RELEASE_PERIODIC ]]; then
   cp -f ${SCRIPT_ROOT}/../EKS_DISTRO_TAG_FILE.yaml ./EKS_DISTRO_TAG_FILE.yaml
   cp -f ${SCRIPT_ROOT}/../eks-distro-base/golang_versions.yaml ./eks-distro-base/golang_versions.yaml
   cp -f ${SCRIPT_ROOT}/../builder-base/versions.yaml ./builder-base/versions.yaml
-  cp -rf ${SCRIPT_ROOT}/../builder-base/checksums ./builder-base/checksums
-  cp -rf ${SCRIPT_ROOT}/../eks-distro-base/make-tests/expected ./eks-distro-base/make-tests/expected/
+  cp -rfv ${SCRIPT_ROOT}/../builder-base/checksums ./builder-base/checksums
+  cp -rfv ${SCRIPT_ROOT}/../eks-distro-base/make-tests/expected ./eks-distro-base/make-tests/expected/
   git add ./EKS_DISTRO_TAG_FILE.yaml
   git add ./eks-distro-base/golang_versions.yaml
   git add ./builder-base/versions.yaml
