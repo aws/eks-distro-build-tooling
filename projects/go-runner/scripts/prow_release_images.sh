8cff5e731e1ea3d0a6429f495202ea85d6e82028
Saurabh Gupta <srbgupta@amazon.com>
2026-02-25 17:44:26 +0000

    minor updates

--- projects/go-runner/scripts/prow_release_images.sh
+++ projects/go-runner/scripts/prow_release_images.sh
@@ -47,5 +47,4 @@ export AWS_CONFIG_FILE=$(pwd)/awscliconfig
 export AWS_PROFILE=ecr-public-push
 unset AWS_ROLE_ARN AWS_WEB_IDENTITY_TOKEN_FILE
 
-
 make -C ${BASE_DIRECTORY}/projects/go-runner "release"
